ecda0ef2b84b99ff1a69c259763b1a1165ca9d31
Ellie Frost <web@stillinbeta.com>
2019-04-16 15:47:57 -0400

    Encode Server-transmitted objects

--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,20 @@ extern crate tokio_codec;
 
 extern crate bytes;
 
-use bytes::BytesMut;
+use bytes::{BufMut, BytesMut};
 use tokio::io::Error;
 use tokio_codec::{Decoder, Encoder};
 
-pub enum IACEvents {
+pub enum ClientEvents {
     IACWillNAWS,
     IACWontNAWS,
+    ResizeEvent(u16, u16),
+    PassThrough(Vec<u8>),
+}
+
+pub enum ServerEvents {
     IACDoNAWS,
     IACDontNAWS,
-    ResizeEvent(u16, u16),
     PassThrough(Vec<u8>),
 }
 
@@ -25,19 +29,88 @@ impl TermionCodec {
 }
 
 impl Encoder for TermionCodec {
-    type Item = IACEvents;
+    type Item = ServerEvents;
     type Error = Error;
 
-    fn encode(&mut self, _item: Self::Item, _dst: &mut BytesMut) -> Result<(), Self::Error> {
-        unimplemented!()
+    fn encode(&mut self, item: Self::Item, dst: &mut BytesMut) -> Result<(), Self::Error> {
+        match item {
+            ServerEvents::IACDontNAWS => {
+                dst.reserve(3);
+                dst.put(&[IAC, DONT, NAWS] as &[u8]);
+            }
+            ServerEvents::IACDoNAWS => {
+                dst.reserve(3);
+                dst.put(&[IAC, DO, NAWS] as &[u8]);
+            }
+            ServerEvents::PassThrough(v) => {
+                dst.reserve(v.len());
+                for b in v {
+                    if b == IAC {
+                        dst.reserve(1);
+                        dst.put(&[IAC, IAC] as &[u8]);
+                    } else {
+                        dst.put(b)
+                    }
+                }
+            }
+        };
+        Ok(())
     }
 }
 
 impl Decoder for TermionCodec {
-    type Item = IACEvents;
+    type Item = ClientEvents;
     type Error = Error;
 
     fn decode(&mut self, _bytes: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
         unimplemented!()
+    }
+}
+
+// https://tools.ietf.org/html/rfc854
+const IAC: u8 = 255;
+const WILL: u8 = 251;
+const WONT: u8 = 252;
+const DO: u8 = 253;
+const DONT: u8 = 254;
+// https://tools.ietf.org/html/rfc1073
+const NAWS: u8 = 31;
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::borrow::Borrow;
+
+    macro_rules! assert_encode_type {
+        ($event: expr, $expected: expr) => {
+            let mut buf = BytesMut::new();
+            TermionCodec::new()
+                .encode($event, &mut buf)
+                .expect("failed to encode");
+            let actual: &[u8] = buf.borrow();
+            assert_eq!($expected, actual);
+        };
+    }
+
+    #[test]
+    fn encode_iac() {
+        assert_encode_type!(ServerEvents::IACDontNAWS, &[IAC, DONT, NAWS]);
+        assert_encode_type!(ServerEvents::IACDoNAWS, &[IAC, DO, NAWS]);
+    }
+
+    #[test]
+    fn encode_payload_normal() {
+        assert_encode_type!(
+            ServerEvents::PassThrough(vec![0x10, 0x20, 0x30]),
+            &[0x10, 0x20, 0x30]
+        );
+    }
+
+    #[test]
+    fn encode_escape() {
+        assert_encode_type!(
+            ServerEvents::PassThrough(vec![0x10, 0xFF, 0x20]),
+            &[0x10, 0xFF, 0xFF, 0x20]
+        );
     }
 }
