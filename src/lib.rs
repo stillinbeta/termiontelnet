63a75c8876d30d92facd2c7e36112498f60c9ce8
Ellie Frost <web@stillinbeta.com>
2019-04-17 13:09:26 -0400

    Working decoder

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
+extern crate bytes;
 extern crate futures;
 extern crate tokio;
 extern crate tokio_codec;
 
-extern crate bytes;
-
-use bytes::{BufMut, BytesMut};
+use bytes::{Buf, BufMut, BytesMut};
 use tokio::io::Error;
 use tokio_codec::{Decoder, Encoder};
 
@@ -12,8 +11,9 @@ use tokio_codec::{Decoder, Encoder};
 pub enum ClientEvents {
     IACWillNAWS,
     IACWontNAWS,
+    // Terminal is now .0 wide and .1 high
     ResizeEvent(u16, u16),
-    PassThrough(Vec<u8>),
+    Byte(u8),
 }
 
 #[derive(Debug)]
@@ -60,17 +60,51 @@ impl Encoder for TermionCodec {
     }
 }
 
+macro_rules! consume_event {
+    ($bytes: expr, $take: expr, $evt: expr) => {{
+        let _ = $bytes.split_to($take);
+        Ok(Some($evt))
+    }};
+}
+
 impl Decoder for TermionCodec {
     type Item = ClientEvents;
     type Error = Error;
 
-    fn decode(&mut self, _bytes: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        unimplemented!()
+    fn decode(&mut self, bytes: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+        if bytes.is_empty() {
+            return Ok(None);
+        }
+        if bytes[0] == IAC {
+            match (bytes.get(1).map(|v| *v), bytes.get(2).map(|v| *v)) {
+                (Some(IAC), _) => consume_event!(bytes, 2, ClientEvents::Byte(IAC)),
+                (Some(WILL), None) | (Some(WONT), None) | (Some(SB), None) => Ok(None),
+                (Some(WILL), Some(NAWS)) => consume_event!(bytes, 3, ClientEvents::IACWillNAWS),
+                (Some(WONT), Some(NAWS)) => consume_event!(bytes, 3, ClientEvents::IACWontNAWS),
+                (Some(SB), Some(NAWS)) if bytes.len() < 9 => Ok(None),
+                (Some(SB), Some(NAWS)) => {
+                    let buf = bytes.split_to(9).freeze();
+                    if let &[IAC, SB, NAWS, w0, w1, h0, h1, IAC, SE] = buf.as_ref() {
+                        let h = u16::from_be_bytes([h0, h1]);
+                        let w = u16::from_be_bytes([w0, w1]);
+                        Ok(Some(ClientEvents::ResizeEvent(h, w)))
+                    } else {
+                        Err(std::io::Error::from(std::io::ErrorKind::InvalidData))
+                    }
+                }
+                _ => Err(std::io::Error::from(std::io::ErrorKind::InvalidData)),
+            }
+        } else {
+            let b0 = bytes.split_to(1).freeze();
+            Ok(Some(ClientEvents::Byte(b0[0])))
+        }
     }
 }
 
 // https://tools.ietf.org/html/rfc854
 const IAC: u8 = 255;
+const SB: u8 = 250; // Subnegotiation Begin
+const SE: u8 = 240; // Subnegotiation End
 const WILL: u8 = 251;
 const WONT: u8 = 252;
 const DO: u8 = 253;
@@ -94,11 +128,13 @@ mod test {
         }};
     }
 
-    // macro_rules! assert_decodes_to {
-    //     $(event: expr, bytes: $expr) => {
-
-    //     }
-    // }
+    macro_rules! assert_decodes_to {
+        ($bytes: expr, $($event: expr),*$(,)?) => {{
+            use futures::Stream;
+            let mut iter = TermionCodec::new().framed(MockStream::new($bytes)).wait();
+            $(assert_eq!($event, iter.next().expect("no next item").expect("task not ready"));)*
+        }};
+    }
 
     #[test]
     fn encode_iac() {
@@ -124,11 +160,49 @@ mod test {
 
     #[test]
     fn decode_iac() {
-        use futures::Stream;
+        assert_decodes_to!(&[IAC, WILL, NAWS], ClientEvents::IACWillNAWS);
+        assert_decodes_to!(&[IAC, WONT, NAWS], ClientEvents::IACWontNAWS);
+    }
 
-        let mut iter = TermionCodec::new()
-            .framed(MockStream::new(&[IAC, WILL, NAWS]))
-            .wait();
-        assert_eq!(ClientEvents::IACWillNAWS, iter.next().unwrap().unwrap())
+    #[test]
+    fn decode_bytes_normal() {
+        assert_decodes_to!(
+            &[10, 20, 30],
+            ClientEvents::Byte(10),
+            ClientEvents::Byte(20),
+            ClientEvents::Byte(30),
+        );
+    }
+
+    #[test]
+    fn decode_bytes_escape() {
+        assert_decodes_to!(
+            &[0x10, 0xFF, 0xFF],
+            ClientEvents::Byte(0x10),
+            ClientEvents::Byte(0xFF),
+        );
+    }
+
+    #[test]
+    fn decode_resize_event() {
+        assert_decodes_to!(
+            &[255, 250, 31, 0, 80, 0, 64, 255, 240],
+            ClientEvents::ResizeEvent(64, 80)
+        )
+    }
+
+    #[test]
+    fn decode_many() {
+        assert_decodes_to!(
+            &[1, 1, 2, 3, IAC, WILL, NAWS, 99, 255, 255, IAC, SB, NAWS, 0, 80, 0, 40, IAC, SE, 20],
+            ClientEvents::Byte(1),
+            ClientEvents::Byte(1),
+            ClientEvents::Byte(2),
+            ClientEvents::Byte(3),
+            ClientEvents::IACWillNAWS,
+            ClientEvents::Byte(99),
+            ClientEvents::Byte(255),
+            ClientEvents::ResizeEvent(40, 80)
+        )
     }
 }
